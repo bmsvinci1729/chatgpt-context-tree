8dd8a3c4e253a6e76922c674682c108040c05258
Sudhanva Bharadwaj BM <bmsv245@gmail.com>
2025-06-19 00:51:02 +0530

    basic echo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,53 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [value, setValue] = useState("");
+  const [subVal, setSubVal] =  useState("Nothing submitted yet");
+  const [messages, setMessages] = useState([]);
+  function onInputChange(e) {
+    // console.log(e.target.value);
+    setValue(e.target.value);
+  }
+
+  function handleClick(e) {
+    console.log("I am Clicked!!")
+    setSubVal(value);
+    setMessages([
+      ...messages,
+      value
+    ])
+  }
+
+
 
   return (
-    <>
+    <div>
+      <h1>Hey, Welcome to the land of ECHO-nversation</h1>
+      <h3>Echo</h3>
+      <input type = "text" placeholder='Type here' value = {value} onChange={onInputChange} /> 
+      <button onClick={handleClick}> Submit</button>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+          <ul>
+            {
+              messages.map((msg)=>{
+                return(
+                  <li>{msg}</li>
+                );
+              })
+
+            }
+          </ul>
+          
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
+
+
+
 }
 
 export default App
