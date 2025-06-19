8255ed88b7085bec4da58fb5ae4577695272ba46
Sudhanva Bharadwaj BM <bmsv245@gmail.com>
2025-06-19 16:40:12 +0530

    basic

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,47 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
 
 function App() {
-
   const [value, setValue] = useState("");
-  const [subVal, setSubVal] =  useState("Nothing submitted yet");
+  const [subVal, setSubVal] = useState("Nothing submitted yet");
   const [messages, setMessages] = useState([]);
+
   function onInputChange(e) {
-    // console.log(e.target.value);
     setValue(e.target.value);
   }
 
   function handleClick(e) {
-    console.log("I am Clicked!!")
+    console.log("I am Clicked!!");
     setSubVal(value);
     setMessages([
       ...messages,
       value
-    ])
+    ]);
   }
-
-
 
   return (
     <div>
       <h1>Hey, Welcome to the land of ECHO-nversation</h1>
       <h3>Echo</h3>
-      <input type = "text" placeholder='Type here' value = {value} onChange={onInputChange} /> 
-      <button onClick={handleClick}> Submit</button>
+      <input
+        type="text"
+        placeholder='Type here'
+        value={value}
+        onChange={onInputChange}
+      />
+      <button onClick={handleClick}>Submit</button>
       <div>
-          <ul>
-            {
-              messages.map((msg)=>{
-                return(
-                  <li>{msg}</li>
-                );
-              })
-
-            }
-          </ul>
-          
+        <ul>
+          {
+            messages.map((msg) => {
+              return (
+                <li>{msg}</li>
+              );
+            })
+          }
+        </ul>
       </div>
     </div>
   );
-
-
-
 }
 
-export default App
+export default App;
